426a61b9195e941365a1b86cac26fd0b370df5ef
Vu Dao <mr.minhvu.dao@gmail.com>
2019-06-30 01:31:24 +0700

    Update nvim
    Add py_plugin.requirements to optimize pip install

--- nvim/to-add/setup_nvim.sh
+++ nvim/to-add/setup_nvim.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 # Setup neovim for container
 
+USER=$(id -u -n)
+
 # Install neovim modules
-pip3 install --user python_language_server
-pip3 install --user neovim
-pip3 install --user pynvim
-pip3 install --user jedi
-pip3 install --user flake8
-pip3 install --user autopep8
-pip3 install --user editorconfig
-pip3 install --user pylint
-pip3 install --user bandit
+pip3 install --user -U -r /home/${USER}/py_plugin.requirements
 
 # Install nvim plugin
-USER=$(id -u -n)
 mkdir -p /home/${USER}/Dev/notes-in-vim
 nvim +PlugInstall +PlugUpdate +PlugUpgrade +qa
 nvim +UpdateRemotePlugins +qa
